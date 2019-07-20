b4c5ad789011d0d91b38e0ec5909d8e27a7bc100
Bej <bej@gmail.com>
2019-07-20 19:16:55 +0530

    Added new changes

--- src/test/java/maven/Trial/TestMaven.java
+++ src/test/java/maven/Trial/TestMaven.java
@@ -11,5 +11,6 @@ public class TestMaven
 {
     public static void main(String[] args){
     	System.out.println("Hello Bejoise. Wish you Good Luck.");
+    	System.out.println("Test");    	
     }
 }
