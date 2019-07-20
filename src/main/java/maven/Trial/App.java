fd382bec0e48a53838d9a705435efcc8b693472f
Bej <bej@gmail.com>
2019-07-20 19:12:36 +0530

    test it

---
+++ src/main/java/maven/Trial/App.java
@@ -0,0 +1,13 @@
+package maven.Trial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/maven/Trial/TestMaven.java
@@ -0,0 +1,15 @@
+package maven.Trial;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestMaven
+{
+    public static void main(String[] args){
+    	System.out.println("Hello Bejoise. Wish you Good Luck.");
+    }
+}
